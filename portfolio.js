96d7748c8f76c936bc0be793ddc3b38f2a3d6b6e
arushigupta16 <arushig16@gmail.com>
2017-06-26 15:00:11 -0400

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,5 @@
 <script>
 window.alert("Welcome to Arushi's page! We are glad to have you!")
-<button onclick="myFunction()">Try it</button>
+
+var button = document.createElement("button");
+button.innerHTML = "About Me";
