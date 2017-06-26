2b6e78ebe3434be55547a41af29aed78e2dbdd72
arushigupta16 <arushig16@gmail.com>
2017-06-26 15:13:03 -0400

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,19 @@
+
+<img onmouseover="bigImg(this)" onmouseout="normalImg(this)"src="Arushi.jpg" alt="Smiley" width="150" height="210">
+  
 <script>
-window.alert("Welcome to Arushi's page! We are glad to have you!")
+window.alert("Welcome to Arushi's page! We are glad to have you!");
 
 var button = document.createElement("button");
 button.innerHTML = "About Me";
+
+  
+  function bigImg(x) {
+    x.style.height = "300px";
+    x.style.width = "240px";
+}
+
+  function normalImg(x) {
+    x.style.height = "210px";
+    x.style.width = "150px";
+}
